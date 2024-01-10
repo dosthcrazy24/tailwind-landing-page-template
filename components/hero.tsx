6eb0fc9def2d4dc27183a17912bbd0a0ae745eb7
dosthcrazy24 <152146507+dosthcrazy24@users.noreply.github.com>
2024-01-11 00:19:52 +0800

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -28,10 +28,10 @@ export default function Hero() {
         <div className="pt-32 pb-12 md:pt-40 md:pb-20"> 
 
           {/* Section header */}
-          <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Creat an amazing <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">套壳App1</span></h1> 
+          <div className="text-center pb-12 md:pb-16"> 
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Create an amazing <br/>套壳App</h1>
             <div className="max-w-3xl mx-auto">  
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">testtest</p>   
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">testtest</p>    
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">  
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://lobe-chat-cyan-nine.vercel.app/#session=inbox">Start Now</a>  
