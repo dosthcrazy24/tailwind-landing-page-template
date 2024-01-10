cfab4819d4e6070c21e85696b6998e9c15c8e15a
dosthcrazy24 <152146507+dosthcrazy24@users.noreply.github.com>
2024-01-10 23:43:33 +0800

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -22,14 +22,14 @@ export default function Hero() {
         </svg>
       </div>
 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6"> 
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">  
 
         {/* Hero content */} 
         <div className="pt-32 pb-12 md:pt-40 md:pb-20"> 
 
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make a great <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">套壳App </span></h1>   
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Creat an amazing <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">套壳App </span></h1>    
             <div className="max-w-3xl mx-auto"> 
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">testtest</p>  
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">  
